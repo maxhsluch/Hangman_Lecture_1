9fccbf3f29cf0aa543188609fad627207029b26e
maxhsluch <maximilian.moebes@stud.hslu.ch>
2025-04-16 14:10:22 +0200

    Erstellen class Hangman
    Beginn mit Erstellung der Klasse Hangman

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,13 @@
 # This is my try of Hangman via Git
 
 print("Welcome to Hangman!")
+
+class Hangman:
+    def __init__(self, word, max_attempts=6):
+        self.word = word.lower()
+        self.guessed_letters = []
+        self.wrong_guesses = 0
+        self.max_attempts = max_attempts
+        
+
+
